38267722098c47a13283e7e812739fb5907ca056
Devon Germano <devon.m.germano@gmail.com>
2023-03-11 19:28:17 -0800

    Updated bullets

--- src/app/portfolio/portfolio-section/portfolio-section-experience/portfolio-section-experience.component.ts
+++ src/app/portfolio/portfolio-section/portfolio-section-experience/portfolio-section-experience.component.ts
@@ -15,10 +15,10 @@ export class PortfolioSectionExperienceComponent implements OnInit {
       logo: 'assets/logos/verb-l.svg',
       color: 'black',
       bullets: [
-        "Led the development of VERB Live, a live streaming, interactive, serverless application that utilized Node.js, Angular, Redis, Kafka, PostgreSQL, and Docker with a variety of 3rd-party integrations in an AWS production environment",
-        "Managed the development of VERB SWIM, a direct-sales enablement platform that utilized Node.js, Nest.js, React, Next.js, and PostgreSQL in an AWS production environment",
-        "Defined project scope and gathered requirements for an architectural migration that saved $15,000 in monthly cloud server costs",
-        "Oversaw a team of 6 and coordinated workflow to delegate technical tasks",
+        "Led the end-to-end development of a flagship product utilizing cutting-edge technologies such as Node.js, Angular, AWS, Redis, PostgreSQL, and Docker, delivering a superior solution to meet customer needs and business objectives.",
+        "Directed and managed a team of 6 skilled engineers in an Agile environment, driving successful project completion and ensuring quality assurance, scalability, and maintainability of the codebase.",
+        "Designed, developed, and maintained serverless applications utilizing AWS Lambda and the Serverless framework, delivering efficient and cost-effective solutions that aligned with the organization's strategic goals.",
+        "Initiated and oversaw the implementation of key architectural improvements to core applications, leveraging best practices and industry standards to achieve optimal performance, security, and scalability.",
       ]
     },
     {
@@ -28,10 +28,9 @@ export class PortfolioSectionExperienceComponent implements OnInit {
       logo: 'assets/logos/sdc-l.svg',
       color: '#5700ff',
       bullets: [
-        "Worked on projects that utilized several different technologies including Python, Vue.js, Node.js, Django, Redis, PostgreSQL, AWS, and Docker in a remote environment",
-        "Collaborated with other developers to identify and implement architectural improvements to existing applications",
-        "Implemented and tested code for user-facing applications using Django and Vue.js",
-        "Developed, tested, and deployed applications in an AWS production environment",
+        "Applied expertise in multiple technologies, such as Python, Vue.js, Node.js, Django, Redis, PostgreSQL, AWS, and Docker, to deliver projects in a remote work environment.",
+        "Collaborated with cross-functional teams to identify and implement architectural enhancements that streamlined operations, increased scalability, and improved system performance.",
+        "Developed and tested user-facing applications using Django and Vue.js, ensuring a seamless user experience and delivering high-quality code that met project specifications and requirements.",
       ]
     },
     {
@@ -41,10 +40,9 @@ export class PortfolioSectionExperienceComponent implements OnInit {
       logo: 'assets/logos/cph-dark.svg',
       color: '#ffbb2d',
       bullets: [
-        "Developed and maintained code in Go, Kotlin, Python, C#, Typescript, and JavaScript in a remote environment",
-        "Designed, implemented, and maintained a continuous integration and continuous delivery pipeline using Kubernetes, Docker, and CircleCI",
-        "Tested, sliced, designed, and implemented mockups for user-facing, front-end applications using Angular 2+",
-        "Implemented serverless applications using AWS and Google Cloud Platform",
+        "Developed and maintained robust, high-quality code in a diverse range of languages, including Go, Kotlin, Python, C#, Typescript, and JavaScript, in a remote work environment.",
+        "Designed, implemented, and maintained an efficient and reliable continuous integration and continuous delivery (CI/CD) pipeline using Docker,and CircleCI.",
+        "Created visually stunning and intuitive user interfaces by testing, slicing, designing, and implementing mockups for front-end applications using Angular 2+.",
       ],
     }
   ];
